67e52ede0b35b6240789091cabc04d6ecd41c98e
DerBot7214 <lengwenath1@gmail.com>
2022-04-13 00:37:48 +0200

    stupido 4.0

--- workflow/updateServer.js
+++ workflow/updateServer.js
@@ -5,6 +5,8 @@ import fetch from 'node-fetch'
 const baseUrl = process.env.PUBLIC_URL
 const url = `${baseUrl}/update/${process.env.SECRET_KEY}`
 
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+
 const run = async () => {
     await fetch(url).then(body => body.text()).then(console.log).catch(console.log)
 
